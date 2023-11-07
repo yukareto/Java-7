c0e2a19338b2403dfd40245fb29cc3b6bf322047
yureto <yut465p@gamil.com>
2023-11-07 23:28:23 +0900

    第7回課題修正3回目

--- src/main/java/com/supergt/racingdriver/response/MessageResponse.java
+++ src/main/java/com/supergt/racingdriver/response/MessageResponse.java
@@ -10,44 +10,5 @@ public class MessageResponse {
     public String getMessage() {
         return message;
     }
-
-    public class RacerCreateResponse {
-
-        private String message;
-
-        public RacerCreateResponse(String message) {
-            this.message = message;
-        }
-
-        public String getMessage() {
-            return message;
-        }
-    }
-
-    public class RacerUpdateResponse {
-
-        private String message;
-
-        public RacerUpdateResponse(String message) {
-            this.message = message;
-        }
-
-        public String getMessage() {
-            return message;
-        }
-    }
-
-    public class RacerDeleteResponse {
-
-        private String message;
-
-        public RacerDeleteResponse(String message) {
-            this.message = message;
-        }
-
-        public String getMessage() {
-            return message;
-        }
-    }
 }
 
